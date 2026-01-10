23c28f27e844ed157e7770c3fb95d2bb68b48f15
moonlight2329 <zainabahamad048@gmail.com>
2026-01-10 20:11:12 +0800

    Update ga.py

--- ga.py
+++ ga.py
@@ -4,61 +4,55 @@ class GeneticAlgorithm:
     def __init__(
         self,
         fitness_func,
-        bounds,
-        pop_size=50,
-        generations=100,
+        chromosome_length,
+        pop_size=30,
+        generations=50,
         crossover_rate=0.8,
-        mutation_rate=0.1
+        mutation_rate=0.05
     ):
         self.fitness_func = fitness_func
-        self.bounds = bounds
+        self.chromosome_length = chromosome_length
         self.pop_size = pop_size
         self.generations = generations
         self.crossover_rate = crossover_rate
         self.mutation_rate = mutation_rate
-
-        self.dim = len(bounds)
         self.history = []
 
     def initialize_population(self):
-        return np.array([
-            [np.random.uniform(low, high) for low, high in self.bounds]
-            for _ in range(self.pop_size)
-        ])
+        return np.random.randint(0, 2, (self.pop_size, self.chromosome_length))
 
     def selection(self, population, fitness):
         idx = np.random.choice(len(population), size=2, replace=False)
         return population[idx[np.argmin(fitness[idx])]]
 
-    def crossover(self, parent1, parent2):
+    def crossover(self, p1, p2):
         if np.random.rand() < self.crossover_rate:
-            point = np.random.randint(1, self.dim)
-            return np.concatenate([parent1[:point], parent2[point:]])
-        return parent1.copy()
+            point = np.random.randint(1, self.chromosome_length)
+            return np.concatenate([p1[:point], p2[point:]])
+        return p1.copy()
 
     def mutate(self, individual):
-        for i in range(self.dim):
+        for i in range(self.chromosome_length):
             if np.random.rand() < self.mutation_rate:
-                low, high = self.bounds[i]
-                individual[i] = np.random.uniform(low, high)
+                individual[i] = 1 - individual[i]
         return individual
 
     def run(self):
         population = self.initialize_population()
 
-        for gen in range(self.generations):
+        for _ in range(self.generations):
             fitness = np.array([self.fitness_func(ind) for ind in population])
             self.history.append(np.min(fitness))
 
-            new_population = []
+            new_pop = []
             for _ in range(self.pop_size):
                 p1 = self.selection(population, fitness)
                 p2 = self.selection(population, fitness)
                 child = self.crossover(p1, p2)
                 child = self.mutate(child)
-                new_population.append(child)
+                new_pop.append(child)
 
-            population = np.array(new_population)
+            population = np.array(new_pop)
 
         best_idx = np.argmin(fitness)
         return population[best_idx], fitness[best_idx], self.history
