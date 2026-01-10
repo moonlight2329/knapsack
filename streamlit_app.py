948d470cc0b3a15637904df60d45bb9597c0ff01
moonlight2329 <zainabahamad048@gmail.com>
2026-01-10 20:22:32 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,21 @@
 import streamlit as st
 import matplotlib.pyplot as plt
-import pandas as pd
 
 from ga import GeneticAlgorithm
-from fitness import feature_selection_fitness, X
+from fitness import knapsack_fitness, values, weights
 
-st.title("Genetic Algorithm – Feature Selection")
+st.title("Genetic Algorithm – Knapsack Optimization")
 
 st.sidebar.header("GA Parameters")
-pop_size = st.sidebar.slider("Population Size", 10, 100, 30)
-generations = st.sidebar.slider("Generations", 10, 200, 50)
+pop_size = st.sidebar.slider("Population Size", 20, 200, 50)
+generations = st.sidebar.slider("Generations", 50, 500, 100)
 mutation_rate = st.sidebar.slider("Mutation Rate", 0.01, 0.3, 0.05)
 crossover_rate = st.sidebar.slider("Crossover Rate", 0.5, 1.0, 0.8)
 
-if st.button("Run GA"):
+if st.button("Run Genetic Algorithm"):
     ga = GeneticAlgorithm(
-        fitness_func=feature_selection_fitness,
-        chromosome_length=X.shape[1],
+        fitness_func=knapsack_fitness,
+        chromosome_length=len(values),
         pop_size=pop_size,
         generations=generations,
         mutation_rate=mutation_rate,
@@ -25,11 +24,13 @@ if st.button("Run GA"):
 
     best_solution, best_fitness, history = ga.run()
 
-    selected_features = X.columns[best_solution == 1]
+    total_value = -best_fitness
+    total_weight = (best_solution * weights).sum()
 
-    st.success("Optimization Complete")
-    st.write("Selected Features:", list(selected_features))
-    st.write("Best Fitness:", best_fitness)
+    st.success("Optimization Completed")
+    st.write("Selected Items:", best_solution)
+    st.write("Total Value:", total_value)
+    st.write("Total Weight:", total_weight)
 
     fig, ax = plt.subplots()
     ax.plot(history)
