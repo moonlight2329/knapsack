5778aa235d5fa025eab135889f67f6343905abdc
moonlight2329 <zainabahamad048@gmail.com>
2026-01-11 09:58:52 +0800

    Update fitness.py

--- fitness.py
+++ fitness.py
@@ -6,16 +6,22 @@ import pandas as pd
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "processed_mknapcb3", "mknapcb3")
 
+def get_available_instances():
+    instances = []
+    for file in os.listdir(DATA_DIR):
+        if file.endswith("_config.json"):
+            instance_id = file.replace("mknapcb3_", "").replace("_config.json", "")
+            instances.append(instance_id)
+    return sorted(instances)
+
 def load_instance(instance_id):
     csv_file = os.path.join(DATA_DIR, f"mknapcb3_{instance_id}.csv")
     json_file = os.path.join(DATA_DIR, f"mknapcb3_{instance_id}_config.json")
 
-    # Load CSV (items)
     df = pd.read_csv(csv_file)
     values = df.iloc[:, 0].astype(float).values
     weights = df.iloc[:, 1].astype(float).values
 
-    # Load JSON (capacity)
     with open(json_file, "r") as f:
         config = json.load(f)
 
